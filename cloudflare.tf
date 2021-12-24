f3d723bbf8f17a3b3ef7e85d0c7790209e4a8740
Charlie Fish <fishcharlie@me.com>
2021-12-24 11:38:38 -0700

    Adding v2, alpha, and beta DNS records

--- cloudflare.tf
+++ cloudflare.tf
@@ -14,10 +14,37 @@ resource "cloudflare_record" "dynamoosejs_com" {
   proxied = true
 }
 
+resource "cloudflare_record" "alpha_dynamoosejs_com" {
+  zone_id = var.zone
+  name    = "alpha.dynamoosejs.com"
+  value   = "alpha.dynamoosejs.com.s3-website-us-west-2.amazonaws.com"
+  type    = "CNAME"
+  ttl     = "1"
+  proxied = true
+}
+
+resource "cloudflare_record" "beta_dynamoosejs_com" {
+  zone_id = var.zone
+  name    = "beta.dynamoosejs.com"
+  value   = "beta.dynamoosejs.com.s3-website-us-west-2.amazonaws.com"
+  type    = "CNAME"
+  ttl     = "1"
+  proxied = true
+}
+
 resource "cloudflare_record" "v1_dynamoosejs_com" {
   zone_id = var.zone
   name    = "v1.dynamoosejs.com"
   value   = "v1.dynamoosejs.com.s3-website-us-west-2.amazonaws.com"
+  type    = "CNAME"
+  ttl     = "1"
+  proxied = true
+}
+
+resource "cloudflare_record" "v2_dynamoosejs_com" {
+  zone_id = var.zone
+  name    = "v2.dynamoosejs.com"
+  value   = "v2.dynamoosejs.com.s3-website-us-west-2.amazonaws.com"
   type    = "CNAME"
   ttl     = "1"
   proxied = true
